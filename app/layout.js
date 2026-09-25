a4f43fcbdb72e04edf04777342cb222788d131c5
tourizm45 <bryanennis@gmail.com>
2026-09-25 09:27:17 -0400

    Build initial Stella author site

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"Stella Recchia | Author",description:"Stories about love, life, and the funny parts in between."};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
